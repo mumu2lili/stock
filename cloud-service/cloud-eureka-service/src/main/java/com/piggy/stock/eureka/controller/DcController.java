560906500fd6777f2fef066eb44eadb487df251d
mumu2lili <451817970@qq.com>
2019-04-17 10:18:09 +0800

    nothing

--- cloud-service/cloud-eureka-service/src/main/java/com/piggy/stock/eureka/controller/DcController.java
+++ cloud-service/cloud-eureka-service/src/main/java/com/piggy/stock/eureka/controller/DcController.java
@@ -1,5 +1,7 @@
 package com.piggy.stock.eureka.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,13 +10,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class DcController {
 
+	private Logger log = LoggerFactory.getLogger(getClass());
+
 	@Autowired
 	DiscoveryClient discoveryClient;
 
 	@GetMapping("/dc")
 	public String dc() {
 		String services = "Services: " + discoveryClient.getServices();
-		System.out.println(services);
+		log.debug(services);
 		return services;
 	}
 
